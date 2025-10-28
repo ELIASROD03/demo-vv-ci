498fe2f0a6b0ac1b4e40becc95ce384e50854d90
Elias Rodriguez <eliasaerg@gmail.com>
2025-10-28 17:29:31 -0600

    Commit inicial con pipeline y prueba exitosa

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function sumar(a, b) {
   return a + b;
 }
 
-// Este "console.log" es solo para que el archivo haga algo.
+
 console.log(sumar(2, 2));
 
 module.exports = sumar; // Exportamos la función para "probarla"
